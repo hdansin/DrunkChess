ca62bd8c6f16e541b0e3a800d3673f7d2be09f72
hdansin <hdansin@mailbox.org>
2021-01-07 11:41:01 -0500

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+function drawBoard () {
+  const lightSquare = '<div class="lightSquare">';
+  const darkSquare = '<div class="darkSquare">';
+  const boardWidth = 8;
+
+  const boardContainer = document.querySelector('.container');
+  for (let i = 0; i < boardWidth; i++) {
+
+  }
+}
