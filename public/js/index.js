0be9badb70d34a27d50d218a94bf5ee59dcdf092
hdansin <hdansin@mailbox.org>
2021-01-21 19:53:00 -0500

    Added castling, en pessant and promotion to queen functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,8 @@
-// Draw the board
+// Start new game
+const chess = new Chess();
+
 function drawBoard () {
+// Draw the board
   console.log('Darwing Board');
 
    const lightSquare = '<div ondrop="drop_handler(event)" ondragover="dragover_handler(event)" ondragstart="dragstart_handler(event)" class="lightSquare"';
@@ -105,19 +108,19 @@ function placePieces () {
     }
     // place white queen
     else if (sq == 'd1') {
-      squareNodes[i].insertAdjacentHTML('afterbegin', '<img id="W" class="piece" src="../assets/images/pieces/wq.svg"/>');
+      squareNodes[i].insertAdjacentHTML('afterbegin', '<img id="Q0" class="piece" src="../assets/images/pieces/wq.svg"/>');
     }
     // place black queen
     else if (sq == 'd8') {
-      squareNodes[i].insertAdjacentHTML('afterbegin', '<img id="q" class="piece" src="../assets/images/pieces/bq.svg"/>');
+      squareNodes[i].insertAdjacentHTML('afterbegin', '<img id="q0" class="piece" src="../assets/images/pieces/bq.svg"/>');
     }
     // place white king
      else if (sq == 'e1') {
-      squareNodes[i].insertAdjacentHTML('afterbegin', '<img id="K" class="piece" src="../assets/images/pieces/wk.svg"/>');
+      squareNodes[i].insertAdjacentHTML('afterbegin', '<img id="K0" class="piece" src="../assets/images/pieces/wk.svg"/>');
     }
     // place black king
      else if (sq == 'e8') {
-      squareNodes[i].insertAdjacentHTML('afterbegin', '<img id="k" class="piece" src="../assets/images/pieces/bk.svg"/>');
+      squareNodes[i].insertAdjacentHTML('afterbegin', '<img id="k0" class="piece" src="../assets/images/pieces/bk.svg"/>');
     }
   }
 }
@@ -126,14 +129,15 @@ function placePieces () {
 function dragstart_handler (event) {
   let parentNode = event.target.parentNode;
   let data = event.target.id + parentNode.id;
-  event.dataTransfer.effectAllowed = "move";
-  event.dataTransfer.dropEffect = "move";
   event.dataTransfer.setData('text/plain', data);
 }
 
 function dragover_handler(event) {
-  event.preventDefault();
-  event.dataTransfer.dropEffect = "move";
+  let data = event.dataTransfer.types;
+  // Only allow drag if it is an image
+  if (data.includes("text/uri-list")) {
+    event.preventDefault();
+  }
 }
 
 function drop_handler(event) {
@@ -145,23 +149,118 @@ function drop_handler(event) {
   // Make sure the drop square is not a piece
   let dropSquare = event.target.className == "piece" ? event.target.parentNode : event.target;
   let piece = document.getElementById(data.slice(0,2));
-
+  let move = {from: '', to: '', promotion: 'q'};
+  move.from = squareFrom;
+  move.to = dropSquare.id;
+  let moveObj = chess.move(move);
+  console.log(moveObj.flags);
   // Check if legal move
-  // clear nodes and insert new piece
-  console.log(dropSquare.hasChildNodes());
-  if (dropSquare.hasChildNodes()) {
-    let oldChild = dropSquare.firstChild;
-    console.log("replacing:" + oldChild);
-    dropSquare.replaceChild(piece, oldChild);
-  } else {
-    dropSquare.appendChild(piece);
-  }
-  // remove piece
-  
+  if (moveObj) {
+    if (moveObj.flags == 'n' || moveObj.flags == 'b') {
+      dropSquare.appendChild(piece);
+    }
+    // clear nodes and insert new piece
+    else if (moveObj.flags == 'c') {
+      let oldChild = dropSquare.firstChild;
+      dropSquare.replaceChild(piece, oldChild);
+    } else {
+      // handle castling
+      if (moveObj.flags == 'k') {
+        // kimgside castle
+        if (moveObj.color == 'w') {
+          // Move King
+          dropSquare.appendChild(piece);
+          // Move rook
+          let R2 = document.getElementById("R2");
+          let f1 = document.getElementById("f1");
+          let h1 = document.getElementById("h1");
+          h1.replaceChildren();
+          f1.appendChild(R2);
+        } else {
+          // Move King
+          dropSquare.appendChild(piece);
+          // Move rook
+          let r2 = document.getElementById("r2");
+          let f8 = document.getElementById("f8");
+          let h8 = document.getElementById("h8");
+          h8.replaceChildren();
+          f8.appendChild(r2);
+
+        }
+      } else if (moveObj.flags == 'q') {
+        // queenside castle
+        if (moveObj.color == 'w') {
+          // Move King
+          dropSquare.appendChild(piece);
+          // Move rook
+          let R1 = document.getElementById("R1");
+          let d1 = document.getElementById("d1");
+          let a1 = document.getElementById("a1");
+          a1.replaceChildren();
+          d1.appendChild(R1);
+        } else {
+          // Move King
+          dropSquare.appendChild(piece);
+          // Move rook
+          let r1 = document.getElementById("r1");
+          let d8 = document.getElementById("d8");
+          let a8 = document.getElementById("a8");
+          a8.replaceChildren();
+          d8.appendChild(r1);
+
+        }
+        // handle en passant
+      } else if (moveObj.flags == 'e') {
+        dropSquare.appendChild(piece);
+        if (moveObj.color == 'w') {
+          //white
+          let sq = document.getElementById(dropSquare.id[0] + (Number(dropSquare.id[1]) - 1));
+          sq.replaceChildren();
+        } else {
+          // black
+          let sq = document.getElementById(dropSquare.id[0] + (Number(dropSquare.id[1]) + 1));
+          sq.replaceChildren();
+        }
+        // handle promotion
+      } else if (moveObj.flags[1] == 'p') {
+        console.log("promotion");
+        let queen = '';
+        let fromNode = document.getElementById(squareFrom);
+        let numQueens = 1;
+        let boardFen = chess.fen();
+        // white 
+        if (moveObj.color == 'w') {
+          for (let i = 0; i < boardFen.length; i++) {
+            if (boardFen[i] == 'w') {
+              i = boardFen.length;
+            } else if (boardFen[i] == 'Q') {
+              numQueens++;
+            }
+          }
+          queen = '<img id="Q' + numQueens + '" class="piece" src="../assets/images/pieces/wq.svg"/>';
+          fromNode.replaceChildren();
+          dropSquare.replaceChildren();
+          dropSquare.insertAdjacentHTML('afterbegin', queen);
+        } else {
+          // black
+          for (let i = 0; i < boardFen.length; i++) {
+            if (boardFen[i] == 'b') {
+              i = boardFen.length;
+            } else if (boardFen[i] == 'q') {
+              numQueens++;
+            }
+          }
+          queen = '<img id="q' + numQueens + '" class="piece" src="../assets/images/pieces/bq.svg"/>';
+          fromNode.replaceChildren();
+          dropSquare.replaceChildren();
+          dropSquare.insertAdjacentHTML('afterbegin', queen);
+        }
+      } 
+    }
+    console.log(chess.ascii());
+    console.log(chess.fen());
+   }
 }
-
-
-
 
 // Draw the board and set up the pieces
 drawBoard();
